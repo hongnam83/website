5f760727f26220a8a09b74cee76d657025573b7c
hongnam83 <hongnam1583@gmail.com>
2026-05-13 08:34:11 +0700

    website

--- src/components/FuranoLogo.tsx
+++ src/components/FuranoLogo.tsx
@@ -1,6 +1,6 @@
 export default function FuranoLogo({ className = "" }: { className?: string }) {
   return (
-    <svg viewBox="0 0 400 120" className={className} fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="0 0 480 120" className={className} fill="none" xmlns="http://www.w3.org/2000/svg">
       {/* Icon */}
       <circle cx="50" cy="35" r="15" fill="#1A2B5E" />
       <path d="M 35 60 A 30 30 0 0 0 90 90 L 90 75 A 15 15 0 0 1 50 60 Z" fill="#3DCAA0" />
